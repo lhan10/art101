156d0d93fa658611992190e31a98fe16d57d7fa0
lhan10 <lhan10@ucsc.edu>
2021-10-14 01:53:30 -0700

    submission

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+// Lab4.
+// Get in the habit of thinking in terms of pseudocoding before jumping into coding.
+// @link   https://lhan10.github.io/art101/
+// @file   This files is for lab4.
+// @author Ligen Han, Wanqi Yan.
+// @since  10/12/2021
+
+
+// Prepare some clean strawberries and fresh milk
+// Get a blender
+  // Pour all the ingredients into the blender
+    //Turn the blender on
+      // Wait for the smoothie to be ready and pour it into a glass
