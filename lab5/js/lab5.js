0d084e44ab811fa79904a96cb4cf866d33d19971
lhan10 <lhan10@ucsc.edu>
2021-10-18 17:47:22 -0700

    submission

---
+++ lab5/js/lab5.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Ligen Han, Wanqi Yan
+ * Created: 10/18/2021
+ * License: public Domain
+ */
+
+// Define Variables
+make = "Farrari";
+model = "458";
+color = "red";
+year = "2021";
+var ownIt = "false";
+
+// calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " year(s)<br>");
+document.writeln("Own it? " + ownIt + "<br>");
